a66b1254ec356148c01bb7eaa3c8717e58651af2
Christian Blank <christian.blank@haw-hamburg.de>
2015-09-09 10:11:18 +0200

    Add session to rest api

---
+++ code/formatters/SessionFormatter.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * The session formatter can format a session into a representation which is consumable from a IRestSerializer.
+ */
+class SessionFormatter implements IRestSerializeFormatter {
+
+    /**
+     * Returns an array with entries for `user`.
+     *
+     * @param Ntb\ApiSession $data
+     * @param array $access
+     * @param array $fields
+     * @return array the user data in a serializable structure
+     */
+    public static function format($data, $access=null, $fields=null) {
+        return [
+            'user' => $data->User->URLSegment,
+            'token' => $data->Token
+        ];
+    }
+}
