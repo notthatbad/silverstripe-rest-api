18aa436ce09415b4364e7360246161b737201096
andrelohmann <lohmann.andre@gmail.com>
2015-10-09 09:02:46 +0200

    removed Memcached Cache; renamed session_cache to rest_cache

--- _config.php
+++ _config.php
@@ -1,18 +1,2 @@
 <?php
 
-SS_Cache::add_backend(
-    'session_cache',
-    'Memcached',
-    array(
-        'servers' => array(
-            'host' => 'localhost',
-            'port' => 11211,
-            'persistent' => true,
-            'weight' => 1,
-            'timeout' => 5,
-            'retry_interval' => 15,
-            'status' => true
-        )
-    )
-);
-SS_Cache::pick_backend('session_cache', 'any', 10);
